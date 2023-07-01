6528f4bd9907f7f3e12de5ba2c8ff95ee197ceb5
nawafx98 <103197174+nawafx98@users.noreply.github.com>
2023-07-02 01:57:20 +0300

    add filtering feature

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def main():
     load_dotenv()
 
     # Access the API key from the environment variable
-    api_key = os.environ["OPENAI_API_KEY"] == st.secrets["OPENAI_API_KEY"]
+    api_key = os.getenv("OPENAI_API_KEY")
 
     # Title and description
     st.title("AskAI App")
@@ -43,8 +43,44 @@ def main():
     data = pd.read_csv(file)
 
     # Display Data Head
-    st.write("Data Preview:")
-    st.dataframe(data.head()) 
+    st.write("Original Data Preview:")
+    st.dataframe(data)
+
+    # Data Filtering
+    st.subheader("Data Filtering")
+    st.write("Note: You can skip the filtering step if you prefer to explore the original data.")
+
+    # Create a copy of the original data
+    filtered_data = data.copy()
+
+    # Display filter options
+    filter_option = st.selectbox("Filter Option \"choose None if you want to skip\"", ["None", "Column Values"])
+
+    # Task: Select Columns for Filtering
+    filter_columns = st.multiselect("Select Columns \"select the filter values for each selected column\"", data.columns)
+
+    # Task: Filter Data based on Column Values
+    filter_values = {}
+    for column in filter_columns:
+        values = data[column].unique()
+        filter_values[column] = st.selectbox(f"Select Filter Value for '{column}'", values, key=column)
+
+    # Apply filters if any
+    if filter_option == "Column Values" and filter_columns:
+        filtered_data = filtered_data.copy()
+        filtered_mask = pd.Series(True, index=filtered_data.index)
+        for column, value in filter_values.items():
+            if value:
+                column_mask = filtered_data[column].astype(str).str.contains(str(value), na=False)
+                filtered_mask &= column_mask
+        filtered_data = filtered_data[filtered_mask]
+
+    # Display note about empty data
+    if filter_option == "Column Values" and filter_columns and filtered_data.empty:
+        st.write("No data matching the filter values.")
+    # Display filtered data
+    st.subheader("Filtered Data Preview:")
+    st.dataframe(filtered_data)
 
     # Define pandas df agent - 0 ~ no creativity vs 1 ~ very creative
     agent = create_pandas_dataframe_agent(OpenAI(temperature=0.1),data,verbose=True) 
@@ -112,6 +148,21 @@ def main():
     if st.button("Clear Conversation"):
         st.session_state.past = []
         st.session_state.generated = []
+    # Export conversation log button
+    if st.button("Export Conversation"):
+        if len(st.session_state.past) > 0:
+            conversation_log = ""
+            for i in range(len(st.session_state.past)):
+                conversation_log += f"User: {st.session_state.past[i]}\n"
+                conversation_log += f"AI Agent: {st.session_state.generated[i]}\n\n"
+            st.download_button(
+                "Download Conversation",
+                data=conversation_log,
+                file_name="conversation.txt",
+                mime="text/plain"
+            )
+        else:
+            st.error("No conversation to export.")
   
 if __name__ == "__main__":
-    main()   
+    main()
